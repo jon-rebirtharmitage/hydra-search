a9c314e97e3d92f4d01e9ead63fa5a5d0e2dd99f
Jonathon Cronin <jon@rebirtharmitage.com>
2018-05-24 14:54:44 -0500

    Angular JS Testing
    Attempting to get Angular JS working to allow for better flexability in this code.

--- popup.js
+++ popup.js
@@ -1,3 +1,30 @@
+/* ------------------------------------------------------- 
+
+* Filename:     Adding Form Fields Dynamically
+* Website:      http://www.shanidkv.com
+* Description:  Shanidkv AngularJS blog
+* Author:       Muhammed Shanid shanidkannur@gmail.com
+
+---------------------------------------------------------*/
+
+var app = angular.module('angularjs-starter', []);
+
+  app.controller('MainCtrl', function($scope) {
+
+  $scope.choices = [{id: 'choice1'}, {id: 'choice2'}];
+  
+  $scope.addNewChoice = function() {
+    var newItemNo = $scope.choices.length+1;
+    $scope.choices.push({'id':'choice'+newItemNo});
+  };
+    
+  $scope.removeChoice = function() {
+    var lastItem = $scope.choices.length-1;
+    $scope.choices.splice(lastItem);
+  };
+  
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   var checkPageButton = document.getElementById('checkPage');
   checkPageButton.addEventListener('click', function() {
